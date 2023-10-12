d0f0f94ad2226323156f338d53009a4a39eafd93
Mia077 <144052943+Mia077@users.noreply.github.com>
2023-10-12 15:01:24 +0300

    Initial commit AnaProject

---
+++ src/main/java/com/demoApplitools/hooks/Hooks.java
@@ -0,0 +1,2 @@
+package com.demoApplitools.hooks;public class Hooks {
+}
---
+++ src/test/java/stepdefinition/LoginStepdefs.java
@@ -0,0 +1,27 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepdefs {
+
+//    @Given("User is on the login page")
+//    public void userIsOnLoginPage() {
+//        System.out.println("User on log page");
+//    }
+@Given("^User is on the login page$")
+public void user_is_on_the_login_page() {
+
+}
+//
+//    @When("User introduce the right id an password")
+//    public void userIntroduceTheRightIdAnPassword() {
+//        System.out.println("Pune password");
+//    }
+//
+//    @Then("User is logged on the page")
+//    public void userIsLoggedOnThePage() {
+//        System.out.println("Sa logat");
+//    }
+
+
+}
